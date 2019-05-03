2fccbf1e66a47ea8c22a6a2fb8fa3851bbfbe6fc
unknown <minho0719@a.ut.ac.kr>
2019-05-03 09:17:12 +0900

    5장연습문제

---
+++ day3/src/pr5/Ex1.java
@@ -0,0 +1,12 @@
+package pr5;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		int[] arr1[];
+		int[] arr2= {1,2,3,};
+		int[] arr3 = new int[5];
+		int[] arr4 = new int[] {1,2,3,4,5};
+		int arr5[];
+		int[] arr6[] = new int[3][];
+	}
+}
