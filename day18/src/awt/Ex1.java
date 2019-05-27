61ff39058b1c6a9bbd1cce4532f520c15b8336c7
mino <minho0719@a.ut.ac.kr>
2019-05-27 23:45:07 +0900

    awt pr

--- day18/src/awt/Ex1.java
+++ day18/src/awt/Ex1.java
@@ -10,64 +10,142 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Ex1 {
-	public static void main(String[] args) {
-		
-		Frame f = new Frame("Bingo");
-		String[] bingo = {"참새","두루미","황새","비둘기","까오기","오리","타조","부엉이",
-				"올빼미","뱁새","꿩","닭","구관조","잉꼬","매","거위","닭","구관조","잉꼬",
-				"매","거위","독수리","콘돌","봉황","공작","까치","까마귀","앵무새","꾀꼬리","고니"};
-		f.setSize(500, 500);
-		f.setLayout(new GridLayout(5, 5));
-		for (int i = 0; i < 25; i++) {
-			Button a = new Button(bingo[i]);
-			a.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					// TODO Auto-generated method stub
-					System.out.println(a.getLabel());
-					if(a.getBackground() == Color.blue)
-						a.setBackground(Color.pink);
-					else
-						a.setBackground(Color.blue);
+	Button[][] arr = new Button[5][5];
+	
+	static int bingoCnt(Button[][] bingo) {
+		int count = 0;
+		int rowAll = 0;
+		int rowEqCol = 0;
+		int rowPlusCol = 0;
+		int[] colArr = new int[bingo[0].length];
+
+		for (int i = 0; i < bingo.length; i++) {
+			for (int j = 0; j < bingo[i].length; j++) {
+				if (bingo[i][j].getBackground() == Color.yellow) {
+					rowAll++;
+					colArr[j]++;
+					if (i == j)
+						rowEqCol++;
+					if (i + j == bingo.length - 1)
+						rowPlusCol++;
 				}
-			});
-			f.add(a);
+			}
+
+			// 행이 빙고일때
+			if (rowAll == bingo[0].length) {
+				System.out.println("행");
+				count++;
+			}
+			rowAll = 0;
+
 		}
-		f.getComponent(0).setBackground(Color.pink);
-		
+
+		// 열 빙고 구하기
+		for (int k = 0; k < colArr.length; k++) {
+			if (colArr[k] == bingo.length) {
+				count++;
+				System.out.println("열");
+			}
+		}
+
+		// 행과 열이 같을때
+		if (rowEqCol == bingo[0].length) {
+			System.out.println("행과열");
+			count++;
+		}
+
+		// 행과열 반대 대각선
+		if (rowPlusCol == bingo[0].length) {
+			System.out.println("행과열 대각");
+			count++;
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		/*
+		 * 1차원 배열25개짜리 만들고 그안에 버튼 하나씩 넣는다. 배열이 체크된걸 알아야 빙고를 체크할수있는데 색이 노란색이면 체크되었다!
+		 */
+		Ex1 bingo1 = new Ex1();
+		Frame f = new Frame("Bingo");
+		String[] bingo = { "참새", "두루미", "황새", "비둘기", "까오기", "오리", "타조", "부엉이", "올빼미", "뱁새", "꿩", "닭", "구관조", "잉꼬", "매",
+				"거위", "독수리", "콘돌", "봉황", "공작", "까치", "까마귀", "앵무새", "꾀꼬리", "고니" };
+		f.setSize(500, 500);
+		f.setLayout(new GridLayout(5, 5)); // 프레임에 그리드 레이아웃 생성
+		// 이벤트가 발생할 때마다 체크를 해주어야함
+		// how??????????????????????????????????????????????? i don't know
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				bingo1.arr[i][j] = new Button(bingo[5 * i + j]);
+				// 버튼 25개만들어서 버튼별 이벤트 추가
+				bingo1.arr[i][j].addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						String btnName = ((Button) e.getSource()).getLabel();
+						System.out.println(btnName);
+						((Button) e.getSource()).setBackground(Color.YELLOW);
+						System.out.println(Ex1.bingoCnt(bingo1.arr));
+					}
+				});
+				f.add(bingo1.arr[i][j]);
+			}
+		}
 		f.addWindowListener(new EventHandler());
+
+		class Thread1 extends Thread {
+			public void run() {
+				while (true) {
+					try {
+						f.setTitle(new SimpleDateFormat("hh:mm:ss a").format(new Date()));
+						Thread.sleep(1000);
+					} catch (Exception e) {
+					}
+				}
+			}
+		}
+		Thread1 th1 = new Thread1();
+		th1.start();
 		f.setVisible(true);
 	}
 }
 
- class EventHandler implements WindowListener, MouseListener , ActionListener{
-	 	public void mouseClicked(MouseEvent e) {
-			// TODO Auto-generated method stub
-			System.out.println(1);
-		}
+class EventHandler implements WindowListener, MouseListener, ActionListener {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+	}
 
-		@Override
-		public void mousePressed(java.awt.event.MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+	}
 
-		@Override
-		public void mouseReleased(java.awt.event.MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
+	@Override
+	public void mousePressed(java.awt.event.MouseEvent e) {
+		// TODO Auto-generated method stub
 
-		@Override
-		public void mouseEntered(java.awt.event.MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
+	}
 
-		@Override
-		public void mouseExited(java.awt.event.MouseEvent e) {}
-	 
+	@Override
+	public void mouseReleased(java.awt.event.MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(java.awt.event.MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(java.awt.event.MouseEvent e) {
+	}
+
 	public void windowOpened(WindowEvent e) {
 	}
 
@@ -92,13 +170,5 @@ public class Ex1 {
 
 	public void windowDeactivated(WindowEvent e) {
 	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	
 
 }
